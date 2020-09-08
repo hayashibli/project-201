af54e7ebf261ca583172cf582194b6bfb68f6531
hayashibli <haya98shibli@gmail.com>
2020-09-08 17:39:06 +0300

    modified files

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 'use strict';
-
+var score=0;
 var username = prompt('What is your name?');
 //console.log('The answer is:', username);
 alert('Welcome to the my webpage ' + username);
@@ -11,6 +11,9 @@ if (myname === 'yes') {
 }
 else if (myname === 'no') {
   alert('Excellent ' + username + ' that\'s corect');
+  // eslint-disable-next-line no-unused-vars
+  score ++;
+  console.log(score);
 }
 
 var university = prompt('Did i study in Jordan?').toLowerCase();
@@ -20,6 +23,8 @@ if (university === 'no') {
 }
 else if (university === 'yes') {
   alert('Excellent ' + username + ' that\'s corect');
+  score ++;
+  console.log(score);
 }
 var major = prompt('Is my major in the medical field').toLowerCase();
 //console.log('The answer is:', major);
@@ -28,6 +33,8 @@ if (major === 'no') {
 }
 else {
   alert('Excellent ' + username + ' that\'s corect');
+  score ++;
+  console.log(score);
 }
 
 var goal = prompt('Is my goal to be an athlete?').toLowerCase();
@@ -37,13 +44,58 @@ if (goal === 'yes') {
 }
 else if (goal === 'no') {
   alert('Excellent ' + username + ' that\'s corect');
+  score ++;
+  console.log(score);
 }
 
 var webpage = prompt('Do you like my webpage?').toLowerCase();
 //console.log(webpage);
 if (webpage === 'yes') {
   alert('Thanks alot ' + username + ' ,hope you enjoyed it :)');
+  score ++;
+  console.log(score);
 }
 else if (webpage === 'no') {
   alert('I respect your opinion ' + username +' :)' );
 }
+
+for (var i=0;i<4;i++) {
+  var num = prompt('Guess a number '+ username);
+  //console.log('The answer is:', num);
+  if(num>25){
+    console.log(num);
+    alert('number is too high!');
+  }
+  else if(num<25){
+    console.log(num);
+    alert('number is too low!');
+  } else{
+    alert('This is the correct answer');
+    score ++;
+    console.log(score);
+    break;} }
+
+
+for (var x=0;x<6;x++){
+  var Q7 = prompt('What types of movies do i like ?').toLowerCase();
+  //console.log('The answer is:', Q7);
+  var movies = ['sciencefiction','romance','comedy'];
+  if(Q7 === movies[0]){
+    alert('Bravo '+ username +' this is one of the options');
+    score ++;
+    console.log(score);
+    alert('I like sciencefiction, romance and comedy');
+    break;}
+  else if(Q7 === movies[1]){
+    alert('Bravo '+ username +' this is one of the options');
+    score ++;
+    console.log(score);
+    alert('I like sciencefiction, romance and comedy'); break;}
+  else if(Q7 === movies[2]){
+    alert('Bravo '+ username +' this is one of the options');
+    score ++;
+    console.log(score);
+    alert('I like sciencefiction, romance and comedy'); break;}
+  else{ alert('No your answer is wrong '+username);}}
+alert('This is your score : '+score+' out of 7'+' '+ ', '+ username );
+
